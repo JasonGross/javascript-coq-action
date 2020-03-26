586ef7001087755992c334da53deaf57497dccfe
Jason Gross <jgross@mit.edu>
2020-03-25 22:11:20 -0400

    First stab at a js action

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+const setup_ocaml = require('avsm/setup-ocaml');
 const wait = require('./wait');
-
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
-
-    core.debug((new Date()).toTimeString())
-    await wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
-
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
-    core.setFailed(error.message);
-  }
+    try {
+        await setup_ocaml.run()
+    }
+    catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run()
